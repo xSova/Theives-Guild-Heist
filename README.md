b72e4a738f3180ab8d75567c7fbeb5347a46b740
Bryce Schultz <25107568+xSova@users.noreply.github.com>
2023-10-31 22:55:06 -0500

    ## Refactor Game Logic, Enhance User Experience, and Remove Obsolete Files
    - Enhanced Game Logic:
    - Implemented a dynamic lose condition by randomly selecting a room as the loss-trigger.
    - Integrated "quit" functionality, allowing players to gracefully exit the game at any point by typing "quit".
    - Improved User Interaction:
    - Updated the user prompts and feedback messages to be more intuitive and informative.
    - Structured the code to handle user input more robustly, managing various actions like 'get item' and 'go direction' more seamlessly.
    - Code Refactoring and Cleanup:
    - Cleaned up and organized the code for improved readability and maintainability.
    - Enhanced the comments within the code, providing more clarity on the game’s flow and logic.
    - Removed Obsolete Files:
    - Deleted TextBasedGame.py and main.py as these files were outdated and no longer necessary for the current implementation of the game.
    - These changes collectively aim to improve the game's overall functionality, user experience, and codebase quality.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-What is this:
-Text based thief / escape game- collect all of the items without getting caught.
+# Thieves' Guild Heist
+
+Welcome to **Thieves' Guild Heist**, a text-based adventure game where you navigate through different maps, steal valuable items, and avoid getting caught. As a member of the illustrious Thieves' Guild, your mission is to infiltrate various locations, snagging as many treasures as you can, and make it out unscathed.
+
+## Description
+
+In this game, players will traverse through randomly selected maps like a Bank, Castle, or even a Rival Thieves' Guild. Each room may contain valuable items to steal or potential danger. Players must choose their paths carefully and decide whether to grab items or proceed to avoid capture.
+
+## Features
+
+- **Random Maps:** Different locations to explore each time you play.
+- **Dynamic Loss Conditions:** Randomly selected rooms where players might get caught, making each game unique.
+- **Interactive Gameplay:** Choose your actions - move between rooms, pick up items, or strategize your exit.
+- **Inventory Management:** Keep track of the items you’ve snagged successfully.
+
+## How to Play
+
+1. **Navigate:** Use the `go (direction)` command to move between rooms.
+2. **Steal Items:** Use the `get (item)` command to pick up items.
+3. **Objective:** Collect as many items as you can and make it back outside without getting caught.
+4. **Commands:**
+   - `go (north/east/west/south)`: Move between rooms.
+   - `get (item_name)`: Pick up an item.
+   - `quit`: Exit the game.
+
+## Installation and Running
+
+1. **Clone the repository:**
+   ```bash
+   git clone https://github.com/xSova/thieves-guild-heist.git
+2. **Navigate to the directory**
+   - `cd thieves-guild-heist`
+3. **Run the game**
+   - `python3 theives-guild-heist.py`
+
+## Contributing
+
+Feel free to fork this repository and make improvements or add new features. Pull requests are welcome!
+
--- theives-guild-heist.py
+++ theives-guild-heist.py
@@ -1,8 +1,11 @@
 import random
 
-# Bryce Schultz
+# Author: Bryce Schultz
+# Define the structure of maps
+
 maps = {
     'Bank': {
+        # Defining the connections between the rooms in the Bank map
         'Entry': {'North': 'Teller Counter', 'East': 'Office', 'West': 'Security Desk', 'South': 'Outside'},
         'Security Desk': {'East': 'Entry', 'North': 'Security Room'},
         'Security Room': {'East': 'Teller Counter', 'South': 'Security Desk'},
@@ -14,6 +17,7 @@ maps = {
         'Vault': {'South': 'Teller Counter', 'North': 'Backroom'}
     },
     'Castle': {
+        # Defining the connections between the rooms in the Castle map
         'Great Hall': {'North': 'Dining Hall', 'West': 'Guard Room', 'East': 'Hallway', 'South': 'Outside'},
         'Bed Chambers': {'West': 'Hallway'},
         'Kitchen': {'South': 'Dining Hall', 'North': 'Outside'},
@@ -27,6 +31,7 @@ maps = {
         'Outside': {'North': 'Great Hall', 'South': 'Kitchen', 'East': 'Staff Chambers'}
     },
     'Rival Thieve\'s Guild': {
+        # Defining the connections between the rooms in the Rival Thieve's Guild map
         'Entry': {'South': 'Outside', 'North': 'Guard Post'},
         'Guard Post': {'South': 'Entry', 'East': 'Dining Hall', 'North': 'Main Hall'},
         'Dining Hall': {'West': 'Guard Post', 'North': 'Social Room'},
@@ -41,6 +46,7 @@ maps = {
 }
 perception = {
     'Bank': {
+        # Defining the items found in each room in the Bank map
         'Entry': 'note',
         'Security Desk': '#1_key',
         'Security Room': '',
@@ -52,6 +58,7 @@ perception = {
         'Vault': 'gold_bars'
     },
     'Castle': {
+        # Defining the items found in each room in the Castle map
         'Great Hall': 'sword',
         'Bed Chambers': 'key',
         'Kitchen': 'gold_spatula',
@@ -65,6 +72,7 @@ perception = {
         'Outside': ''
     },
     'Rival Thieve\'s Guild': {
+        # Defining the items found in each room in the Rival Thieve's Guild map
         'Entry': 'map',
         'Guard Post': '',
         'Dining Hall': 'guild-member\'s_bag',
@@ -77,74 +85,77 @@ perception = {
         'Main Hall': 'guild_secrets'
     }
 }
+
+# Randomly choosing a map to play
 current_map_name = random.choice(list(maps))
 rooms = maps[current_map_name]
 print(current_map_name)
 current_map_items = perception[current_map_name]
-lose_con = ['Guard Post', 'Staff Chambers', 'Security Desk']
+
+# Dictionary to store the loss condition room for each map
+lose_con = {}
+
+# Initializing player's inventory and user input
 player_inventory = []
 user_input = []
 
 
 def main():
     global player_inventory, user_input
-    current_room = 'Outside'  # default room
+    current_room = 'Outside'  # Start the player outside the target location.
+
+    # Welcome the player and explain the rules.
     print('Welcome to the Thieve\'s Guild. Today your target is the ' + current_map_name)
     print('To move, type \'go (direction)\' and to take an item, type \'get (item)\'')
     print('Get all the items you can, then go back outside without getting caught.')
 
-    while True:
-        possible_directions = ', '.join(rooms[current_room].keys())
+    while True:  # Main game loop where the player can continue taking actions.
 
-        if current_room == 'Outside':
-            if (len(player_inventory)) == (len(current_map_items) - 2):
-                print('You\'ve escaped with all the items! Good work!')
-                break
-            user_input = input(f'You are outside. Where would you like to go? {possible_directions}:\n').split()
-            current_room = rooms[current_room][user_input[1]]
-            if user_input[0] == "Quit":
-                break
-            if user_input[1] not in possible_directions:
-                print('Direction invalid.')
-                user_input = input(f'Choose from go/get ({possible_directions})(item):\n').split()
-        elif current_room not in lose_con:
-            if user_input[0] in ['Quit', 'quit']:
-                break
-            print('You are in the', current_room + f'. You see: {current_map_items[current_room]}')
-            user_input = input(
-                f'What would you like to do? (You can go: {possible_directions} or get(item):\n').split()
-            if user_input[0] == "Quit":
-                break
-            if user_input[0] in ['get', 'Get']:
-                if user_input[1] != current_map_items[current_room]:
-                    print('Error can not get that item.')
-                    user_input = input(
-                        f'What would you like to do? (You can go: {possible_directions} or get(item):\n').split()
-                elif user_input[1] in player_inventory:
-                    print(f'You already have {user_input[1]} in your inventory: {player_inventory}.')
-                    user_input = input(
-                        f'What would you like to do? (You can go: {possible_directions} or get(item):\n').split()
-                else:
-                    player_inventory.append(user_input[1])
-                    print(f'{user_input[1]} successfully added to inventory.')
-                    print(f'Inventory: {player_inventory}')
-                    user_input = input(
-                        f'What would you like to do? (You can go: {possible_directions} or get(item):\n').split()
-            if user_input[0] in ['go', 'Go']:
-                if user_input[1] not in possible_directions:
-                    print('Direction invalid.')
-                    user_input = input(f'Choose from go/get ({possible_directions})(item):\n').split()
-                current_room = rooms[current_room][user_input[1]]
-            if user_input[0] not in ['go', 'get', 'Go', 'Get']:
-                print('Error, command invalid.')
-                user_input = input(f'Choose from go/get ({possible_directions})(item):\n').split()
-            if user_input[1] not in possible_directions:
-                print('Direction invalid.')
-                user_input = input(f'Choose from go/get ({possible_directions})(item):\n').split()
-        else:
+        # Check for lose condition
+        if current_room == lose_con:
             print('You were caught! Game over.')
             break
-    print('Thanks for playing!')
 
+        possible_directions = ', '.join(rooms[current_room].keys())  # Getting possible directions.
 
+        # Display room info and available actions
+        print(f'You are in the {current_room}. You can go: {possible_directions}')
+        if current_map_items[current_room]:
+            print(f'You see: {current_map_items[current_room]}')
+
+        # Get user input for action
+        user_input = input('What would you like to do? ').strip().lower()
+
+        # Check if user wants to quit the game.
+        if user_input == 'quit':
+            print('You chose to exit the game. Goodbye!')
+            break
+
+        user_input = user_input.split()
+
+        # Process user action: getting an item
+        if user_input[0].lower() == 'get':
+            item = user_input[1]
+            if item == current_map_items[current_room]:
+                player_inventory.append(item)
+                print(f'You have added {item} to your inventory.')
+            else:
+                print(f'There is no item "{item}" in this room.')
+
+        # Process user action: moving to a different room
+        elif user_input[0].lower() == 'go':
+            direction = user_input[1].capitalize()
+            if direction in rooms[current_room]:
+                current_room = rooms[current_room][direction]
+            else:
+                print(f'You cannot go {direction} from here.')
+        else:
+            print('Invalid action. Please use "go [direction]" or "get [item]".')
+
+        # Check for the winning condition
+        if current_room == 'Outside' and len(player_inventory) == len(current_map_items) - 1:
+            print('Congratulations! You’ve escaped with all the items. You win!')
+            break
+
+# Call the main function to start the game
 main()
